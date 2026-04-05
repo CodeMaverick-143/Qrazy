90b4aacf8f3fa2fef32b6bf2f0483bcfe76dc289
CodeMaverick-143 <arpit.01.sarang.2005@gmail.com>
2026-04-05 21:13:25 +0530

    fix: stabilize frontend serve path and cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,8 +20,9 @@ module.exports = {
     {
       name: 'qrazy-frontend',
       script: 'serve',
+      cwd: './client',
       env: {
-        PM2_SERVE_PATH: 'client/dist',
+        PM2_SERVE_PATH: './dist',
         PM2_SERVE_PORT: 3000,
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: './index.html'
